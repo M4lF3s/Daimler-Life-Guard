7927b568b2e6ccad3364888eefe25533ecb81c17
David Bauske <david.bauske@googlemail.com>
2016-04-07 17:00:22 +0200

    Road is default hidden now

--- presentation/js/lifeguard.js
+++ presentation/js/lifeguard.js
@@ -8,13 +8,13 @@ $.extend($.easing, {
 });
 
 var car = {
-    command: false,
-    lastState: false,
-    animating: false,
-    element: null,
-    road: null,
-    roadPosition: 0,
-    animateCalls: 0,
+  command: false,
+  lastState: false,
+  animating: false,
+  element: null,
+  road: null,
+  roadPosition: 0,
+  animateCalls: 0,
 	
 	hazardFlasherEnabled: false,
 	hazardFlasherOn: false
@@ -32,66 +32,66 @@ $(document).ready(function() {
 });
 
 function adjustSlidesSize() {
-    var slidesWidth = $(window).width() - $('.motorway').width();
-    $('.slides').width(slidesWidth)
+  var slidesWidth = $(window).width() - $('.motorway').width();
+  $('.slides').width(slidesWidth)
 }
 
 function roadCam(scale, translate) {
-    if(translate == null) {
-        translate = 0;
-    }
-    $('.road').css('transform', 'scale(' + scale + ',' + scale + ') translateX(-' + translate + 'px)');
+  if(translate == null) {
+      translate = 0;
+  }
+  $('.road').css('transform', 'scale(' + scale + ',' + scale + ') translateX(-' + translate + 'px)');
 }
 
 function runCar() {
-    car.command = true;
-    if(!car.animating) {
-        animStep(true);
-    }
+  car.command = true;
+  if(!car.animating) {
+      animStep(true);
+  }
 }
 
 function stopCar() {
-    car.command = 0;
+  car.command = 0;
 }
 
 function animStep(manual) {
-    var stepDuration = 1000;
-    var stepSize = 25;
-    
-    if(manual == null) {
-        ++car.animateCalls;
-        if(car.animateCalls % car.lanes.length != 0) {
-            return;
-        }
-    }
-    
-    if(car.command) {
-        // Car should move
-        if(car.lastState) {
-            // Car was already moving -> just move on
-            car.roadPosition += stepSize;
-            car.lanes.animate({
-                'background-position-y': car.roadPosition + '%'
-            }, stepDuration, 'linear', animStep);
-        } else {
-            // Car was not moving -> Accelerate
-            car.roadPosition += 50;
-            car.lanes.animate({
-                'background-position-y': car.roadPosition + '%'
-            }, 4000, 'easeInQuad', animStep);
-            car.lastState = true;
-        }
-    } else {
-        // Car should stand still
-        if(car.lastState) {
-            // Car was moving -> stop it
-            car.lastState = false;
-            car.roadPosition += 30;
-            car.lanes.animate({
-                'background-position-y': car.roadPosition + '%'
-            }, 3000, 'easeOutQuad', animStep);
-        }
-    }
+  var stepDuration = 1000;
+  var stepSize = 25;
+
+  if(manual == null) {
+      ++car.animateCalls;
+      if(car.animateCalls % car.lanes.length != 0) {
+          return;
+      }
+  }
+
+  if(car.command) {
+      // Car should move
+      if(car.lastState) {
+          // Car was already moving -> just move on
+          car.roadPosition += stepSize;
+          car.lanes.animate({
+              'background-position-y': car.roadPosition + '%'
+          }, stepDuration, 'linear', animStep);
+      } else {
+          // Car was not moving -> Accelerate
+          car.roadPosition += 50;
+          car.lanes.animate({
+              'background-position-y': car.roadPosition + '%'
+          }, 4000, 'easeInQuad', animStep);
+          car.lastState = true;
+      }
+  } else {
+      // Car should stand still
+      if(car.lastState) {
+          // Car was moving -> stop it
+          car.lastState = false;
+          car.roadPosition += 30;
+          car.lanes.animate({
+              'background-position-y': car.roadPosition + '%'
+          }, 3000, 'easeOutQuad', animStep);
+      }
+  }
 }
 function enableHazardSystem(enable){
 	car.hazardFlasherEnabled = enable;
@@ -113,18 +113,28 @@ function changeCarImageBlink(){
 }
 
 function changeLane(dir) {
-    var ROAD_WIDTH = 200;
-    var ROTATION = 6;
-    var TIMEOUT = 700;
-    
-    var car = $('.car');
-    
-    car.css('transform', 'rotate(' + dir * ROTATION + 'deg)');
-    
-    var oldLeft = parseInt(car.css('left'));
-    car.css('left', oldLeft + dir * ROAD_WIDTH);
-    
-    setTimeout(function() {
-        car.css('transform', 'none');
-    }, TIMEOUT);
+  var ROAD_WIDTH = 200;
+  var ROTATION = 6;
+  var TIMEOUT = 700;
+
+  var car = $('.car');
+
+  car.css('transform', 'rotate(' + dir * ROTATION + 'deg)');
+
+  var oldLeft = parseInt(car.css('left'));
+  car.css('left', oldLeft + dir * ROAD_WIDTH);
+
+  setTimeout(function() {
+      car.css('transform', 'none');
+  }, TIMEOUT);
+}
+
+function showRoad() {
+  $('.motorway').animate({width: 300}, 500);
+  $('.slides').animate({width: $(window).width() - 300}, 500);
+}
+
+function hideRoad() {
+  $('.motorway').animate({width: 0}, 500);
+  $('.slides').animate({width: $(window).width()}, 500);
 }
