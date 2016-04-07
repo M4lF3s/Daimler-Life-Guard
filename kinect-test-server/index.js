0f80702243c594e99331b264bb5ceeb9fc999eff
David Bauske <david.bauske@googlemail.com>
2016-04-07 17:00:37 +0200

    Added uni bayreuth logo

--- kinect-test-server/index.js
+++ kinect-test-server/index.js
@@ -1,7 +1,6 @@
 http = require('http');
 fs = require('fs');
 server = http.createServer( function(req, res) {
-
     console.dir(req.param);
 
     if (req.method == 'POST') {
@@ -17,10 +16,9 @@ server = http.createServer( function(req, res) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end('post received');
     }
-
 });
 
 port = 3000;
-host = '127.0.0.1';
+host = '0.0.0.0';
 server.listen(port, host);
 console.log('Listening at http://' + host + ':' + port);
