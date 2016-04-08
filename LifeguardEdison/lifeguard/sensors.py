1d2ff8b95a61d3f71c42a7661efedb6ab0c889ce
Richard Jasinski <asdfdude133@gmail.com>
2016-04-08 20:09:32 +0200

    updated scripts for edison

--- LifeguardEdison/lifeguard/sensors.py
+++ LifeguardEdison/lifeguard/sensors.py
@@ -15,5 +15,40 @@ class EKG(multiprocessing.Process):
     def run(self):
         while True:
             value = self.adc.read()
-            self.queue.put(value)
+            self.queue.put((time.time(), value))
             time.sleep(0.002)
+
+
+class MuscleActivity(multiprocessing.Process):
+    def __init__(self, queue, pin):
+        super(MuscleActivity, self).__init__()
+        self.queue = queue
+        self.adc = mraa.Aio(pin)
+        self.adc.setBit()
+        os.nice(-20)
+
+    def run(self):
+        while True:
+            value = self.adc.read()
+            self.queue.put(value)
+            time.sleep(0.02)
+
+
+class Acc(multiprocessing.Process):
+    def __init__(self, queue):
+        super(Acc, self).__init__()
+        self.queue = queue
+        self.i2c = mraa.I2c(6)
+        self.i2c.address(0x1D)
+        if self.i2c.readReg(0x0D) != 0x2A:
+            raise RuntimeError("Acc not connected.")
+
+        self.i2c.writeReg(0x0E, 3)  # set scale
+        self.i2c.writeReg(0x2A, 1)  # set active
+        os.nice(-20)
+
+    def run(self):
+        while True:
+            acc_raw = self.i2c.readBytesReg(0x01, 6)
+            self.queue.put(acc_raw)
+            time.sleep(0.02)
