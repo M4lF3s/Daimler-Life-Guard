6bbdeac88342e22ca4058bec81c9db407fc08b6a
David Bauske <david.bauske@googlemail.com>
2016-04-08 13:38:19 +0200

    Added sails measurement controller

---
+++ LifeGuardServer/api/controllers/MeasurementsController.js
@@ -0,0 +1,21 @@
+/**
+ * MeasurementsController
+ *
+ * @description :: Server-side logic for managing Measurements
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+  maxData: 1000,
+  sensorData: [],
+
+	post: function(req, res) {
+    var self = this;
+    self.sensorData = [req.body].concat(self.sensorData);
+    if(self.sensorData.length > self.maxData) {
+      self.sensorData.pop();
+    }
+    console.log(self.sensorData);
+  }
+};
+
---
+++ presentation/js/dashboard.js
@@ -0,0 +1,12 @@
+/**
+ * Created by David on 08.04.2016.
+ */
+$(document).ready(function () {
+  setInterval(pollValues, 100);
+});
+
+function pollValues() {
+  $.get('', function(data) {
+    
+  });
+}
